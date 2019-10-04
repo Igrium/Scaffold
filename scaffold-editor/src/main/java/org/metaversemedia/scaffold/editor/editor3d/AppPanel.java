5e380d0d631cb6312585033c73484919de047e69
Sam54123 <thesam54123@gmail.com>
2019-10-03 22:00:15 -0700

    wrote editor asset loading system

--- scaffold-editor/src/main/java/org/metaversemedia/scaffold/editor/editor3d/AppPanel.java
+++ scaffold-editor/src/main/java/org/metaversemedia/scaffold/editor/editor3d/AppPanel.java
@@ -13,6 +13,7 @@ import com.jme3.system.JmeCanvasContext;
  * Represents the 3d editor app in a panel.
  * @author Sam54123
  */
+@SuppressWarnings("serial")
 public class AppPanel extends JPanel {
 
 	EditorWindow parent;
@@ -30,26 +31,27 @@ public class AppPanel extends JPanel {
 	 * Restarts if already active.
 	 */
 	public void start() {
-		if (app != null) {
-			app.stop();
+		if (app == null) {
+			AppSettings settings = new AppSettings(true);
+			settings.setWidth(getWidth());
+			settings.setHeight(getHeight());
+			
+			app = new EditorApp(parent);
+			app.setSettings(settings);
+			app.createCanvas();	
+			
+			JmeCanvasContext ctx = (JmeCanvasContext) app.getContext();
+			ctx.setSystemListener(app);
+			Dimension dim = new Dimension(getWidth(), getHeight());
+			ctx.getCanvas().setPreferredSize(dim);
+			
+			this.add(ctx.getCanvas());
+			
+			app.startCanvas();
+		} else {
+			app.reload();
 		}
 		
-		AppSettings settings = new AppSettings(true);
-		settings.setWidth(getWidth());
-		settings.setHeight(getHeight());
-		
-		app = new EditorApp(parent);
-		app.setSettings(settings);
-		app.createCanvas();	
-		
-		JmeCanvasContext ctx = (JmeCanvasContext) app.getContext();
-		ctx.setSystemListener(app);
-		Dimension dim = new Dimension(getWidth(), getHeight());
-		ctx.getCanvas().setPreferredSize(dim);
-		
-		this.add(ctx.getCanvas());
-		
-		app.startCanvas();
 	}
 
 }
--- scaffold-editor/src/main/java/org/metaversemedia/scaffold/editor/editor3d/EditorApp.java
+++ scaffold-editor/src/main/java/org/metaversemedia/scaffold/editor/editor3d/EditorApp.java
@@ -1,12 +1,20 @@
 package org.metaversemedia.scaffold.editor.editor3d;
 
+import java.nio.file.Path;
+
 import org.metaversemedia.scaffold.editor.ui.EditorWindow;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.ClasspathLocator;
+import com.jme3.asset.plugins.FileLocator;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
+import com.jme3.texture.Texture.MagFilter;
 
 /**
  * The 3d rendered part of the editor.
@@ -37,12 +45,35 @@ public class EditorApp extends SimpleApplication {
 	public void simpleInitApp() {
 		// activate windowed input behavior
 		flyCam.setDragToRotate(true);
-
+		flyCam.setMoveSpeed(10);
+		
+		// load assets
+		org.metaversemedia.scaffold.core.AssetManager scaffoldAssetManager = parent.getProject().assetManager();
+		for (Path p : scaffoldAssetManager.getLoadedPaths()) {
+			getAssetManager().registerLocator(p.resolve("assets").toString(), FileLocator.class);
+		}
+		
+		getAssetManager().registerLocator("assets", ClasspathLocator.class); // Default assets
+		
+		reload();
+	}
+	
+	/**
+	 * Reload the level from scratch.
+	 */
+	public void reload() {
+		restart();
+		for (Spatial n : rootNode.getChildren()) {
+			n.removeFromParent();
+		}
+		
 		Box b = new Box(1, 1, 1);
 		Geometry geom = new Geometry("Box", b);
 
 		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat.setColor("Color", ColorRGBA.Blue);
+		Texture tex = assetManager.loadTexture("minecraft/textures/block/stone.png");
+		tex.setMagFilter(MagFilter.Nearest);
+		mat.setTexture("ColorMap", tex);
 		geom.setMaterial(mat);
 
 		rootNode.attachChild(geom);
