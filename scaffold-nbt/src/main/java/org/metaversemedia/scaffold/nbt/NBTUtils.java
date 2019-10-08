526261e799b338f57273c9840b4ea6d543404116
Sam54123 <thesam54123@gmail.com>
2019-10-08 09:25:42 -0700

    started working on nbtToString method

--- scaffold-nbt/src/main/java/org/metaversemedia/scaffold/nbt/NBTUtils.java
+++ scaffold-nbt/src/main/java/org/metaversemedia/scaffold/nbt/NBTUtils.java
@@ -1,10 +1,43 @@
 package org.metaversemedia.scaffold.nbt;
 
+import java.util.Iterator;
+
+import com.flowpowered.nbt.CompoundMap;
+import com.flowpowered.nbt.CompoundTag;
+import com.flowpowered.nbt.Tag;
+import com.flowpowered.nbt.TagType;
+
 /**
  * Utility class for common nbt functions
  * @author Sam54123
  *
  */
 public class NBTUtils {
+	/**
+	 * Convert nbt to a string.
+	 * @param nbt NBT input.
+	 * @return Generated string.
+	 */
+	public static String nbtToString(CompoundMap nbt) {
+		if (nbt == null) {
+			return "";
+		}
+		String finalString = "{";
+		
+		Iterator<Tag<?>> tagIterator = nbt.iterator();
+		
+		while (tagIterator.hasNext()) {
+			Tag<?> tag = tagIterator.next();
+			
+			String tagValue = null;
+			if (tag.getType() == TagType.TAG_BYTE_ARRAY) {
+			}
+		}
+		
+		return finalString;
+	}
 	
+	public static String byteArrayToString(byte[] byteArray) {
+		return new String(byteArray);
+	}
 }
---
+++ scaffold-nbt/src/test/java/org/metaversemedia/scaffold/test/NBTTest.java
@@ -0,0 +1,24 @@
+package org.metaversemedia.scaffold.test;
+
+import org.junit.Test;
+import org.metaversemedia.scaffold.nbt.NBTUtils;
+
+import com.flowpowered.nbt.CompoundMap;
+import com.flowpowered.nbt.FloatTag;
+import com.flowpowered.nbt.StringTag;
+
+public class NBTTest {
+
+	@Test
+	public void test() {
+		System.out.println("test");
+		CompoundMap testMap = new CompoundMap();
+		testMap.put(new StringTag("TestTag","test"));
+		testMap.put(new FloatTag("FloatTag",5.0f));
+		
+		System.out.println(NBTUtils.nbtToString(testMap));
+		
+		assert(true);
+	}
+
+}
