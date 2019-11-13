6ed3f178a2897d2dd92c590fd489260f7889df9d
Sam54123 <thesam54123@gmail.com>
2019-11-13 12:17:54 -0800

    started work on chunk reader

--- scaffold-nbt/src/main/java/org/scaffoldeditor/nbt/io/WorldInputStream.java
+++ scaffold-nbt/src/main/java/org/scaffoldeditor/nbt/io/WorldInputStream.java
@@ -3,14 +3,15 @@ package org.scaffoldeditor.nbt.io;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.flowpowered.nbt.CompoundMap;
+
 /**
  * This class can read and parse Minecraft Region files,
- * outputing them into a series of CompoundMaps.
+ * outputting them into a series of CompoundMaps.
  * @author Sam54123
  */
 public class WorldInputStream implements Closeable {
@@ -28,11 +29,60 @@ public class WorldInputStream implements Closeable {
 		 */
 		public final short length;
 		
-		public ChunkLocation(int offset, short length) {
+		/**
+		 * The X position in the region file of the chunk.
+		 */
+		public final short x;
+		
+		/**
+		 * The Z position in the region file of the chunk.
+		 */
+		public final short z;
+		
+		public ChunkLocation(int offset, short length, short x, short z) {
 			this.offset = offset;
 			this.length = length;
+			this.x = x;
+			this.z = z;
 		}
 		
+	}
+	
+	/**
+	 * Represents data about a chunk once it's been read.
+	 */
+	public class ChunkNBTInfo {
+		
+		/**
+		 * The chunk's NBT data.
+		 */
+		public final CompoundMap NBT;
+		
+		/**
+		 * How many bytes were read while reading the chunk?
+		 */
+		public final int bytesRead;
+		
+		/**
+		 * The chunk's X position in the file.
+		 */
+		public final short x;
+		
+		/**
+		 * The chunk's Z position in the file.
+		 */
+		public final short z;
+		
+		/**
+		 * For internal use only.
+		 * DO NOT CALL MANUALLY.
+		 */
+		public ChunkNBTInfo(CompoundMap NBT, int bytesRead, short x, short z) {
+			this.NBT = NBT;
+			this.bytesRead = bytesRead;
+			this.x = x;
+			this.z = z;
+		}
 	}
 	
 	private final InputStream is;
@@ -41,7 +91,14 @@ public class WorldInputStream implements Closeable {
 	private int offsetX;
 	private int offsetz;
 	
-	public List<ChunkLocation> chunkLocations = new ArrayList<ChunkLocation>();
+	// The current head location.
+	private short headX;
+	private short headZ;
+	
+	// The head in the chunkLocations array.
+	private int locationHead;
+	
+	public final List<ChunkLocation> chunkLocations = new ArrayList<ChunkLocation>();
 	
 	/**
 	 * Create a new WorldInputStream.
@@ -52,20 +109,18 @@ public class WorldInputStream implements Closeable {
 		this.is = is;
 		
 		// Load chunk data.
-		int x = 0;
-		int z = 0;
+		short x = 0;
+		short z = 0;
 		
-		// Iterate over chunk array
+		// Iterate over chunk array and read location entries.
 		while (x < 32 && z < 32) {
 			byte[] bytes = new byte[4];
 			is.read(bytes);
-			System.out.println(Arrays.toString(bytes));
 			
 			int loc = (bytes[2] & 0xFF) | ((bytes[1] & 0xFF) << 8) | ((bytes[0] & 0x0F) << 16);
-			
 			short length = bytes[3];
 			
-			chunkLocations.add(new ChunkLocation(loc, length));
+			chunkLocations.add(new ChunkLocation(loc, length, x, z));
 			
 			// Increment chunk heads
 			if (x == 31) {
@@ -76,6 +131,25 @@ public class WorldInputStream implements Closeable {
 			}
 		}
 		
+		// Skip past timestamps
+		is.skip(4096);
+	}
+	
+//	// Read the next chunk from the file.
+//	public ChunkNBTInfo readChunkNBT() {
+//		
+//	}
+	
+	/**
+	 * Increment the head to the next position.
+	 */
+	private void increment() {
+		if (headX == 31) {
+			headX = 0;
+			headZ++;
+		} else {
+			headX++;
+		}
 	}
 
 	@Override
--- scaffold-nbt/src/test/java/org/metaversemedia/scaffold/test/NBTTest.java
+++ scaffold-nbt/src/test/java/org/metaversemedia/scaffold/test/NBTTest.java
@@ -15,11 +15,13 @@ public class NBTTest {
 //			WorldInputStream is = new WorldInputStream(new FileInputStream("C:\\Users\\Sam54123\\Documents\\Minecraft\\MapdevUtils\\Scaffold\\testProject\\game\\saves\\Test World\\region\\r.0.0.mca"));
 			WorldInputStream is = new WorldInputStream(new FileInputStream("/Users/h205p1/Documents/Scaffold-Workspace/testProject/game/saves/TestWorld/region/r.0.0.mca"));
 			for (ChunkLocation c : is.chunkLocations) {
-				System.out.println(c.offset+", "+c.length);
+				System.out.println(c.offset+", "+c.length+", "+c.x+", "+c.z);
 				if (c.length > 0) {
 					count++;
 				}
 			}
+			
+			is.close();
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
