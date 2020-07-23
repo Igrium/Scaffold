b05501055a5edb90752d64d428fecd70e96968eb
Sam54123 <thesam54123@gmail.com>
2020-07-23 00:19:52 -0700

    finished theory code for block mesh renderer

--- scaffold-editor/src/main/java/org/scaffoldeditor/editor/editor3d/blockmodel/BlockMesh.java
+++ scaffold-editor/src/main/java/org/scaffoldeditor/editor/editor3d/blockmodel/BlockMesh.java
@@ -7,11 +7,15 @@ import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.scaffoldeditor.editor.editor3d.blockmodel.ModelElement.Face;
+import org.scaffoldeditor.editor.editor3d.blockmodel.ModelElement.CullFace;
 import org.scaffoldeditor.editor.editor3d.util.EditorUtils;
 
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer.Type;
+import com.jme3.util.BufferUtils;
 
 /**
  * A mesh used to store there rendering data of a block model.
@@ -19,23 +23,19 @@ import com.jme3.scene.Mesh;
  */
 public class BlockMesh extends Mesh {
 	
-	/**
-	 * Represents different sides of a block mesh.
-	 */
-	public enum MeshFaceSide {
-			UP, DOWN, NORTH, SOUTH, EAST, WEST, MASTER
-	}
 	
 	private class CreateFaceReturn {
 		public List<Vector2f> texCoords;
 		public List<Integer> indices;
 	}
 	
+	private List<ModelElement> elements;
+	
 	// Keep track of everything that creates a mesh in different groups so they can be turned off.
 	// texCoord and indices are stored with respect to local array.
-	private Map<MeshFaceSide, List<Vector3f>> vertexGroups;
-	private Map<MeshFaceSide, List<Vector2f>> texCoordGroups;
-	private Map<MeshFaceSide, List<Integer>> indexGroups;
+	private Map<ModelElement.CullFace, List<Vector3f>> vertexGroups;
+	private Map<ModelElement.CullFace, List<Vector2f>> texCoordGroups;
+	private Map<ModelElement.CullFace, List<Integer>> indexGroups;
 	
 	public BlockMesh() {};
 	
@@ -45,85 +45,149 @@ public class BlockMesh extends Mesh {
 	 * taken from the elements tag of a <a href=https://minecraft.gamepedia.com/Model#Block_models>block model.</a> 
 	 */
 	public BlockMesh(JSONArray elements) {
-		
+		for (int i = 0; i < elements.length(); i++) {
+			loadElement(elements.getJSONObject(i));
+		}
+		recompileElements();
 	}
+	
+	/**
+	 * Get a list of all the elements in the model.
+	 * @return Mutable list of all elements.
+	 */
+	public List<ModelElement> getElements() {
+		return elements;
+	}
+	
+	/**
+	 * Re-compile all the elements in the block mesh into their sets of vertices.
+	 */
+	public void recompileElements() {
+		clear();
+		for (ModelElement e : elements) {
+			compileElement(e);
+		}
+		
+		/**
+		 * Insure this object can be used by vanilla JME methods.
+		 */
+		compileMesh(this, true, true, true, true, true, true);
+	}
+	
+	/**
+	 * Compile the block mesh into a mesh readable by JME
+	 * @param mesh Mesh to compile into. WILL CLEAR BUFFERS
+	 * @param upVisible Is the top face visible?
+	 * @param downVisible Is the bottom face visible?
+	 * @param northVisible Is the north face visible?
+	 * @param southVisible Is the south face visible?
+	 * @param eastVisible Is the east face visible?
+	 * @param westVisible Is the west face visible?
+	 */
+	public void compileMesh(Mesh mesh, boolean upVisible, boolean downVisible,
+			boolean northVisible, boolean southVisible, boolean eastVisible, boolean westVisible) {
+		
+		List<Vector3f> vertBuffer = new ArrayList<Vector3f>();
+		List<Vector2f> texCoordBuffer = new ArrayList<Vector2f>();
+		List<Integer> indexBuffer = new ArrayList<Integer>();
+		
+		if (upVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.UP),  texCoordGroups.get(CullFace.UP), indexGroups.get(CullFace.UP));
+		}
+		
+		if (downVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.DOWN),  texCoordGroups.get(CullFace.DOWN), indexGroups.get(CullFace.DOWN));
+		}
+		
+		if (northVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.NORTH),  texCoordGroups.get(CullFace.NORTH), indexGroups.get(CullFace.NORTH));
+		}
+		
+		if (southVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.SOUTH),  texCoordGroups.get(CullFace.SOUTH), indexGroups.get(CullFace.SOUTH));
+		}
+		
+		if (eastVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.EAST),  texCoordGroups.get(CullFace.EAST), indexGroups.get(CullFace.EAST));
+		}
+		
+		if (westVisible) {
+			addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.WEST),  texCoordGroups.get(CullFace.WEST), indexGroups.get(CullFace.WEST));
+		}
+		
+		addVertsToBuffers(vertBuffer, texCoordBuffer, indexBuffer, vertexGroups.get(CullFace.NONE),  texCoordGroups.get(CullFace.NONE), indexGroups.get(CullFace.NONE));
+		
+		mesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertBuffer.toArray(new Vector3f[vertBuffer.size()])));
+		mesh.setBuffer(Type.TexCoord, 2, BufferUtils.createFloatBuffer(texCoordBuffer.toArray(new Vector2f[texCoordBuffer.size()])));
+		mesh.setBuffer(Type.Index, 3, BufferUtils.createIntBuffer(integerListToInt(indexBuffer)));
+		mesh.updateBound();
+	}
+	
+	private int[] integerListToInt(List<Integer> list) {
+		int[] array = new int[list.size()];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = list.get(i);
+		}
+		return array;
+	}
+	
 	
 	/**
 	 * Load an element onto the block mesh.
 	 * @param element Element to load.
 	 */
 	protected void loadElement(JSONObject element) {
-		Vector3f from = EditorUtils.jsonArrayToMVector(element.getJSONArray("from"));
-		Vector3f to = EditorUtils.jsonArrayToMVector(element.getJSONArray("to"));
-		
-		JSONObject faces = element.getJSONObject("faces");
-		
-		// TODO: SUPPORT FACE TEXTURE ROTATION
+		elements.add(new ModelElement(element));
 	}
 	
 	/**
-	 * Finish the process of creating a face from a list of vertices.
-	 * @param vertices The input vertices going counterclockwise.
-	 * @param texCoords The texture coordinates to apply to the plane. Scheme: [x1, y1, x2, y2]
-	 * @return The generated values with respect to the input array.
+	 * Compile a model element onto the model.
+	 * @param element Element to load
 	 */
-	private CreateFaceReturn createFace(List<Vector3f> vertices, float[] texCoords) {
-		CreateFaceReturn ret = new CreateFaceReturn();
-		ret.texCoords = new ArrayList<Vector2f>();
-		
-		// TODO: Make sure UVs are being added correctly.
-		if (texCoords != null) {
-			ret.texCoords.add(new Vector2f(texCoords[0], texCoords[1]));
-			ret.texCoords.add(new Vector2f(texCoords[2], texCoords[1]));
-			ret.texCoords.add(new Vector2f(texCoords[2], texCoords[3]));
-			ret.texCoords.add(new Vector2f(texCoords[0], texCoords[3]));
-		} else {
-			ret.texCoords.add(new Vector2f(0, 0));
-			ret.texCoords.add(new Vector2f(1, 0));
-			ret.texCoords.add(new Vector2f(1, 1));
-			ret.texCoords.add(new Vector2f(0, 1));
+	protected void compileElement(ModelElement element) {
+		appendFace(element.getUpFace());
+		appendFace(element.getDownFace());
+		appendFace(element.getNorthFace());
+		appendFace(element.getSouthFace());
+		appendFace(element.getEastFace());
+		appendFace(element.getWestFace());
+	}
+	
+	/**
+	 * Clear all the compiled mesh data.
+	 * ONLY CALL IF YOU KNOW WHAT YOU'RE DOING
+	 */
+	protected void clear() {
+		vertexGroups.clear();
+		texCoordGroups.clear();
+		indexGroups.clear();
+	}
+	
+	private void appendFace(Face face) {
+		if (face != null) {
+			appendVerts(face.vertices, face.texCoords, face.indices, face.cullFace);
 		}
-		
-		
-		ret.indices = new ArrayList<Integer>();
-		
-		ret.indices.addAll(Arrays.asList(new Integer[] {0,1,3}));
-		ret.indices.addAll(Arrays.asList(new Integer[] {3,1,2}));
-		
-		return ret;
 	}
 	
 	/**
-	 * Create the top face of an element.
-	 * @param face 
-	 * @param from
-	 * @param to
-	 */
-	private void createUpFace(JSONObject face, Vector3f from, Vector3f to) {
-		List<Vector3f> vertices = vertexGroups.get(MeshFaceSide.UP);
-		int offset = vertices.size();
-		
-		// Create vertices of the face.
-		List<Vector3f> localVerts = Arrays.asList(new Vector3f[] {
-				new Vector3f(from.x, to.y, from.z),
-				new Vector3f(to.x, to.y, from.z),
-				to,
-				new Vector3f(from.x, to.y, to.z)
-		});
-		
-		CreateFaceReturn genFace = createFace(localVerts, jsonToFloatArray(face.optJSONArray("uv")));
-		
-		appendVerts(localVerts, genFace.texCoords, genFace.indices, MeshFaceSide.UP);
-	}
-	
-	/**
-	 * Append a set of faces to the model.
+	 * Append a set of vertices to the model.
 	 * @param vertices
 	 * @param texCoord UV coordinates.
 	 * @param indices
 	 * @param side The side to apply it to.
 	 */
-	private void appendVerts(List<Vector3f> vertices, List<Vector2f> texCoord, List<Integer> indices, MeshFaceSide side) {
+	private void appendVerts(List<Vector3f> vertices, List<Vector2f> texCoord, List<Integer> indices, ModelElement.CullFace side) {
+		// Make sure mesh maps are initialized.
+		if (!vertexGroups.containsKey(side)) {
+			vertexGroups.put(side, new ArrayList<Vector3f>());
+		}
+		if (!texCoordGroups.containsKey(side)) {
+			texCoordGroups.put(side, new ArrayList<Vector2f>());
+		}
+		if (!indexGroups.containsKey(side)) {
+			indexGroups.put(side, new ArrayList<Integer>());
+		}
+		
 		List<Vector3f> meshVerts = vertexGroups.get(side);
 		List<Vector2f> meshTexCoord = texCoordGroups.get(side);
 		List<Integer> meshIndices = indexGroups.get(side);
@@ -141,6 +205,23 @@ public class BlockMesh extends Mesh {
 		}
 		
 		meshIndices.addAll(correctedIndices);
+	}
+	
+	private void addVertsToBuffers(List<Vector3f> vertBuffer, List<Vector2f> texCoordBuffer, List<Integer> indexBuffer,
+			List<Vector3f> vertices, List<Vector2f> texCoord, List<Integer> indices) {
+		// index offset of buffers.
+		int offset = vertBuffer.size();
+		
+		vertBuffer.addAll(vertices);
+		texCoordBuffer.addAll(texCoord);
+		
+		// Clone indices so we don't screw up the passed list
+		List<Integer> correctedIndices = new ArrayList<Integer>(indices);
+		for (int i = 0; i < correctedIndices.size(); i++) {
+			correctedIndices.set(i, correctedIndices.get(i) + offset);
+		}
+		
+		indexBuffer.addAll(correctedIndices);
 	}
 	
 	private static float[] jsonToFloatArray(JSONArray in) {
--- scaffold-editor/src/main/java/org/scaffoldeditor/editor/editor3d/blockmodel/ModelElement.java
+++ scaffold-editor/src/main/java/org/scaffoldeditor/editor/editor3d/blockmodel/ModelElement.java
@@ -1,10 +1,16 @@
 package org.scaffoldeditor.editor.editor3d.blockmodel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  * Represents a single model element.
@@ -15,6 +21,10 @@ public class ModelElement {
 	
 	public enum RotationAxis {
 		X, Y, Z
+	}
+	
+	public enum CullFace {
+		UP, DOWN, NORTH, SOUTH, EAST, WEST, NONE
 	}
 	
 	/**
@@ -36,58 +46,299 @@ public class ModelElement {
 		/**
 		 * Specifies the angle of rotation. Can be 45 through -45 degrees in 22.5 degree increments.
 		 */
-		public float angle;
+		public float angle = 0.0f;
 		
 		/**
 		 * Specifies whether or not to scale the faces across the whole block. Can be true or false. Defaults to false.
 		 */
-		public boolean rescale;
+		public boolean rescale = false;
 	}
 	
-
 	/**
-	 * Create a model element.
-	 * @param from Start point of the cube.
-	 * @param to End point of the cube.
-	 * @param rotation Rotation of the element.
-	 * @param faces Holds all the faces of the cube. If a face is left out, it does not render.
+	 * Represents a face as a raw set of indexed points.
+	 * @author Igrium
 	 */
-	public ModelElement(Vector3f from, Vector3f to, ElementRotation rotation, JSONObject faces) {
-		
+	public static class Face {
+		public List<Vector3f> vertices;
+		public List<Integer> indices;
+		public List<Vector2f> texCoords;
+		public CullFace cullFace = CullFace.NONE;
 	}
+	
+	protected ElementRotation rotation;
+	protected JSONObject faces;
+	
+	/**
+	 * The start point of the cube.
+	 */
+	protected Vector3f from;
+	
+	/**
+	 * The end point of the cube.
+	 */
+	protected Vector3f to;
+	
+	/**
+	 * The spatial that assists the element rotation.
+	 */
+	private Spatial rotatedSpatial = new Node();
 	
 	/**
 	 * Create a model element from a JSON object.
 	 * @param element Element JSON object.
 	 */
 	public ModelElement(JSONObject element) {
-		this(jsonArrayToVector(element.getJSONArray("from")), jsonArrayToVector(element.getJSONArray("to")),
-				null, element.getJSONObject("faces"));
 		
-		ElementRotation rotation = new ElementRotation();
-		JSONObject rotationTag = element.getJSONObject("rotation");
-		rotation.origin = jsonArrayToVector(rotationTag.getJSONArray("origin"));
-		rotation.angle = element.getFloat("angle");
-		rotation.rescale = element.getBoolean("rescale");
-		
-		String axisString = element.getString(element.getString("axis"));
-		if (axisString == "x") {
-			rotation.axis = RotationAxis.X;
-		} else if (axisString == "y") {
-			rotation.axis = RotationAxis.Y;
-		} else if (axisString == "z") {
-			rotation.axis = RotationAxis.Z;
+		// Parse rotation
+		rotation = new ElementRotation();
+		JSONObject rotationTag = element.optJSONObject("rotation");
+		if (rotationTag != null) {
+			rotation.origin = jsonArray3ToVector3(rotationTag.getJSONArray("origin"));
+			rotation.angle = element.getFloat("angle");
+			rotation.rescale = element.getBoolean("rescale");
+			
+			String axisString = element.getString(element.getString("axis"));
+			if (axisString.matches("x")) {
+				rotation.axis = RotationAxis.X;
+			} else if (axisString.matches("y")) {
+				rotation.axis = RotationAxis.Y;
+			} else if (axisString.matches("z")) {
+				rotation.axis = RotationAxis.Z;
+			}
 		}
 		
 		
+		// Parse from and to
+		from = jsonArray3ToVector3(element.getJSONArray("from"));
+		to = jsonArray3ToVector3(element.getJSONArray("to"));
+		
+		faces = element.getJSONObject("faces");
+		
+		setupRotation();
 	}
 	
 	/**
-	 * The JSON object containing all the face data.
+	 * Get the start point of the cube.
+	 * @return From value.
 	 */
-	protected JSONObject faces;
+	public Vector3f getFrom() {
+		return from;
+	}
 	
-	private static Vector3f jsonArrayToVector(JSONArray array) {
+	/**
+	 * Get the to value of the cube.
+	 * @return To value.
+	 */
+	public Vector3f getTo() {
+		return to;
+	}
+	
+	/**
+	 * Get the up face of this element.
+	 * @return The up face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getUpFace() {
+		JSONObject face = faces.optJSONObject("up");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(from.x, to.y, from.z)),
+				getGlobalPosition(new Vector3f(to.x, to.y, from.z)),
+				getGlobalPosition(to),
+				getGlobalPosition(new Vector3f(from.x, to.y, to.z))
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the down face of this element.
+	 * @return The down face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getDownFace() {
+		JSONObject face = faces.optJSONObject("down");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(from.x, from.y, from.z)),
+				getGlobalPosition(new Vector3f(from.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, from.z))
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the north face of this element.
+	 * @return The north face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getNorthFace() {
+		JSONObject face = faces.optJSONObject("north");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(to.x, to.y, from.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, from.z)),
+				getGlobalPosition(new Vector3f(from.x, from.y, from.z)),
+				getGlobalPosition(new Vector3f(from.x, to.y, from.z))
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the south face of this element.
+	 * @return The south face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getSouthFace() {
+		JSONObject face = faces.optJSONObject("south");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(from.x, to.y, to.z)),
+				getGlobalPosition(new Vector3f(from.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, to.y, to.z))
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the west face of this element.
+	 * @return The west face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getWestFace() {
+		JSONObject face = faces.optJSONObject("west");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(from.x, to.y, from.z)),
+				getGlobalPosition(new Vector3f(from.x, from.y, from.z)),
+				getGlobalPosition(new Vector3f(from.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(from.x, to.y, to.z))
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the east face of this element.
+	 * @return The east face. NULL IF SHOULD NOT BE RENDERED
+	 */
+	public Face getEastFace() {
+		JSONObject face = faces.optJSONObject("east");
+		if (face == null) {
+			return null;
+		}
+		
+		List<Vector3f> verts = Arrays.asList(new Vector3f[] {
+				getGlobalPosition(new Vector3f(to.x, to.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, to.z)),
+				getGlobalPosition(new Vector3f(to.x, from.y, from.z)),
+				getGlobalPosition(new Vector3f(to.x, to.y, from.z)),
+
+		});
+		
+		return createFace(verts, face);
+	}
+	
+	/**
+	 * Get the global position of a point with the element rotation taken into account.
+	 * @param point Position of the point WITHOUT element rotation.
+	 * @return Position of the point WITH element rotation.
+	 */
+	protected Vector3f getGlobalPosition(Vector3f point) {
+		// Get position relative to rotation root to prevent undesired translation.
+		Vector3f localPoint = point.subtract(rotation.origin);
+		
+		return rotatedSpatial.localToWorld(localPoint, null);		
+	}
+	
+	/**
+	 * Finish the process of creating a face from a list of vertices.
+	 * @param vertices The input vertices going counterclockwise.
+	 * @param face The JSON object representing the face in the file.
+	 * @return The generated values with respect to the input array.
+	 */
+	protected Face createFace(List<Vector3f> vertices, JSONObject face) {
+		Face ret = new Face();
+		ret.texCoords = new ArrayList<Vector2f>();
+		ret.vertices = vertices;
+		
+		JSONArray texCoords = face.optJSONArray("uv");
+		
+		// TODO: Make sure UVs are being added correctly.
+		if (texCoords != null) {
+			ret.texCoords.add(new Vector2f(texCoords.getFloat(0), texCoords.getFloat(1)));
+			ret.texCoords.add(new Vector2f(texCoords.getFloat(2), texCoords.getFloat(1)));
+			ret.texCoords.add(new Vector2f(texCoords.getFloat(2), texCoords.getFloat(3)));
+			ret.texCoords.add(new Vector2f(texCoords.getFloat(0), texCoords.getFloat(3)));
+		} else {
+			ret.texCoords.add(new Vector2f(0, 0));
+			ret.texCoords.add(new Vector2f(1, 0));
+			ret.texCoords.add(new Vector2f(1, 1));
+			ret.texCoords.add(new Vector2f(0, 1));
+		}		
+		
+		ret.indices = new ArrayList<Integer>();
+		
+		ret.indices.addAll(Arrays.asList(new Integer[] {0,1,3}));
+		ret.indices.addAll(Arrays.asList(new Integer[] {3,1,2}));
+		
+		// Parse cullface.
+		String cullfaceString = face.optString("cullface");
+		
+		if (cullfaceString.matches("up")) {
+			ret.cullFace = CullFace.UP;
+		} else if (cullfaceString.matches("down")) {
+			ret.cullFace = CullFace.DOWN;
+		} else if (cullfaceString.matches("east")) {
+			ret.cullFace = CullFace.EAST;
+		} else if (cullfaceString.matches("west")) {
+			ret.cullFace = CullFace.WEST;
+		} else if (cullfaceString.matches("north")) {
+			ret.cullFace = CullFace.NORTH;
+		} else if (cullfaceString.matches("south")) {
+			ret.cullFace = CullFace.SOUTH;
+		} else {
+			ret.cullFace = CullFace.NONE;
+		}
+		
+		return ret;
+	}
+	
+	/**
+	 * Called on construction to setup the rotation of the internal spatial
+	 */
+	private void setupRotation() {
+		if (rotation != null) {
+			if (rotation.axis == RotationAxis.X) {
+				rotatedSpatial.rotate(rotation.angle, 0, 0);
+			} else if (rotation.axis == RotationAxis.Y) {
+				rotatedSpatial.rotate(0, rotation.angle, 0);
+			} else if (rotation.axis == RotationAxis.Z) {
+				rotatedSpatial.rotate(0, 0, rotation.angle);
+			}
+			
+			rotatedSpatial.setLocalTranslation(rotation.origin);
+		}
+	}
+	
+	private static Vector3f jsonArray3ToVector3(JSONArray array) {
 		return new Vector3f(array.getFloat(0), array.getFloat(1), array.getFloat(2));
 	}
+	
+	
 }
